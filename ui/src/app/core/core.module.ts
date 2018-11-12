eac9c0b464b15a2a802580ae9657725ebed5e5fe
nikage <42967644+nikage@users.noreply.github.com>
2018-11-12 16:39:39 +0200

    update deps (#9)
    * int ui app
    
    * add .sccs styles preprocessor
    
    * add sccs styles support
    
    * update angular
    
    * create routing module
    
    * create core module
    
    * create create shared module
    
    * add t

---
+++ ui/src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    //  nav component etc
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
+
+export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
+  if (parentModule) {
+    throw new Error(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
+  }
+}
