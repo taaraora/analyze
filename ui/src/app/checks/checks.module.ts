8142d1084663910afbcd768a96d1b23ab4cb7f7a
Nick <nikage23+1@gmail.com>
2018-11-15 21:23:55 +0200

    add styles to checks page

--- ui/src/app/checks/checks.module.ts
+++ ui/src/app/checks/checks.module.ts
@@ -1,10 +1,10 @@
 import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ChecksRoutingModule }                              from './checks-routing.module';
-import { ChecksComponent }                                  from './checks.component';
-import { HttpClientModule }                                 from "@angular/common/http";
-import { MatCardModule, MatTabsModule, MatExpansionModule } from "@angular/material";
+import { ChecksRoutingModule }                                             from './checks-routing.module';
+import { ChecksComponent }                                                 from './checks.component';
+import { HttpClientModule }                                                from "@angular/common/http";
+import { MatCardModule, MatTabsModule, MatExpansionModule, MatIconModule } from "@angular/material";
 
 @NgModule({
   declarations: [ChecksComponent],
@@ -15,6 +15,7 @@ import { MatCardModule, MatTabsModule, MatExpansionModule } from "@angular/mater
     MatCardModule,
     MatTabsModule,
     MatExpansionModule,
+    MatIconModule,
   ],
 })
 export class ChecksModule {
